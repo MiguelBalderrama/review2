c75f6ea5f3428cccd6d437b32b9173cc2a500aa2
Ian Edgeley <ian@iedgeley.com>
2014-10-22 08:22:00 -0500

    Review python file added

---
+++ review.py
@@ -0,0 +1,20 @@
+###############################
+#                             #
+#           Review 2!         #
+#                             #
+###############################
+
+# Write a program that uses a loop to print out:
+
+#           2
+#           4
+#           6
+#           8
+# Who do we appreciate?
+
+# Use a loop to complete this, either a for loop or a while loop.
+# Each time through the loop, print out either 2, 4, 6 or 8.
+# When the loop is finished, print "Who do we appreciate?"
+# Remember how range can be used to create the list [2,4,6,8]
+# When complete, commit to github and submit a pull request!
+
